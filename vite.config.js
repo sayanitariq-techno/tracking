546d9c5ea5baf6d8ac9a7a661cce3be18548a418
sayanitariq-techno <sayanitariq@gmail.com>
2025-08-22 12:34:11 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// IMPORTANT: base ko aapke repo naam se match hona chahiye
-export default defineConfig({
-  plugins: [react()],
-  base: '/tracking/',
-})
